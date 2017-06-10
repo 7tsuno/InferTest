77a3d356b51108bc7c3b5dd7ad9404bd7be2f0d4
nakayama <hoge@fuga.jpo>
2017-06-11 02:52:43 +0900

    a

--- tested/src/main/java/infertest/tested/logic/Logic.java
+++ tested/src/main/java/infertest/tested/logic/Logic.java
@@ -4,14 +4,8 @@ public class Logic {
 
 	public void test1() {
 
-		DAO dao = new DAO();
-
-		if (dao.getBeanOrNot() == null) {
-
-			DAO dao2 = new DAO();
-			dao2.getBeanOrNot().getCode();
-
-		}
+		DAO dao2 = new DAO();
+		dao2.getBeanOrNot().getCode();
 
 		DAO dao3 = new DAO();
 		dao3.getBeanOrNot().getCode();
