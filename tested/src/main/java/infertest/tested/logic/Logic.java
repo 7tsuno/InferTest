8ad7735ae17a53872bf3a6dcba005b9f9cc78882
nakayama <hoge@fuga.jpo>
2017-06-12 00:39:25 +0900

    a

--- tested/src/main/java/infertest/tested/logic/Logic.java
+++ tested/src/main/java/infertest/tested/logic/Logic.java
@@ -12,14 +12,20 @@ public class Logic {
 
 		test1(bean, null);
 
+		Bean bean2 = test2("str");
+
+		bean2.getCode();
+
+	}
+
+	private Bean test2(String string) {
+
+		return dao.getBeanOrNot("bbb");
+
 	}
 
 	public void test1(Bean bean, String str) {
 
-		str = null;
-		System.out.println(bean.getCode().length());
-
-		str.length();
 	}
 
 }
