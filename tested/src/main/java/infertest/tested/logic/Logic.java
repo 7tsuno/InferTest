00212ab8a98fef87c297414b8a22eb49a94d822d
nakayama <hoge@fuga.jpo>
2017-06-11 21:41:50 +0900

    a

--- tested/src/main/java/infertest/tested/logic/Logic.java
+++ tested/src/main/java/infertest/tested/logic/Logic.java
@@ -2,6 +2,10 @@ package infertest.tested.logic;
 
 public class Logic {
 
+	public static void main(String[] args) {
+		new Logic().test1();
+	}
+
 	public void test1() {
 
 		DAO dao = new DAO();
@@ -10,7 +14,7 @@ public class Logic {
 		if (dao.getBeanOrNot2() != null) {
 
 			DAO dao3 = new DAO();
-			dao.setI(3);
+			dao3.setI(3);
 			dao3.getBeanOrNot2().getCode();
 		}
 
