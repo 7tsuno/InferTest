765647477c9a2f64d4ff6c6bf36c9c44b764fb89
nakayama <hoge@fuga.jpo>
2017-06-12 00:17:15 +0900

    a

--- tested/src/main/java/infertest/tested/logic/Logic.java
+++ tested/src/main/java/infertest/tested/logic/Logic.java
@@ -1,12 +1,18 @@
 package infertest.tested.logic;
 
+import infertest.tested.bean.Bean;
+
 public class Logic {
 
 	private DAO dao;
 
 	public void test1() {
 
-		if (dao.getBeanOrNot("aaa") != null) {
+		Bean bean = dao.getBeanOrNot("aaa");
+
+		bean.getCode();
+
+		if (bean != null) {
 
 			dao.getBeanOrNot("bbb").getCode();
 
