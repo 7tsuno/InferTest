54a82294e9c97b6541d855d4af41087ed9b8ffb3
nakayama <a@a>
2017-06-12 14:10:33 +0900

    a

--- other/src/main/java/infertest/other/logic/OtherLogic.java
+++ other/src/main/java/infertest/other/logic/OtherLogic.java
@@ -1,33 +1,42 @@
 package infertest.other.logic;
 
+import infertest.other.bean.OtherBean;
 import lombok.Data;
 
 @Data
 public class OtherLogic {
 
-	private boolean check;
+    private boolean check;
 
-	public static String returnNullStatic() {
-		return null;
-	}
+    public static String returnNullStatic() {
+        return null;
+    }
 
-	public String returnNullInstance() {
-		return null;
-	}
+    public String returnNullInstance() {
+        return null;
+    }
 
-	public String returnWithCheck() {
+    public String returnWithCheck() {
 
-		return check ? "abc" : null;
+        return check ? "abc" : null;
 
-	}
+    }
 
-	public int returnLength(String str) {
-		return str.length();
-	}
+    public int returnLength(String str) {
+        return str.length();
+    }
 
-	public int returnLengthWithCheck(String str) {
+    public int returnLengthWithCheck(String str) {
 
-		return check ? 0 : str.length();
-	}
+        return check ? 0 : str.length();
+    }
+
+    public void a() {
+
+        OtherBean b = new OtherBean();
+        b.setCode(null);
+        b.getCode().length();
+
+    }
 
 }
