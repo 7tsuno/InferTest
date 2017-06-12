fe22ed826898cc44a773192902410297831f1d88
nakayama <a@a>
2017-06-12 12:24:31 +0900

    a

--- tested/src/main/java/infertest/tested/logic/InterfaceLogic.java
+++ tested/src/main/java/infertest/tested/logic/InterfaceLogic.java
@@ -1,12 +1,9 @@
 package infertest.tested.logic;
 
-import javax.annotation.Nullable;
-
 import infertest.tested.bean.Bean;
 
 public interface InterfaceLogic {
 
-    @Nullable
     Bean find(String str);
 
 }
