cde4682c3f0e07f09e44aa01ea4de7068243d8b4
nakayama <a@a>
2017-06-12 14:54:45 +0900

    a

--- tested/src/main/java/infertest/tested/logic/Logic.java
+++ tested/src/main/java/infertest/tested/logic/Logic.java
@@ -3,6 +3,7 @@ package infertest.tested.logic;
 import java.util.Arrays;
 import java.util.List;
 
+import infertest.other.logic.OtherLogic;
 import infertest.tested.bean.Bean;
 import infertest.tested.bean.Bean2;
 
@@ -122,4 +123,11 @@ public class Logic {
 
     }
 
+    public void test13() {
+
+        OtherLogic lo = new OtherLogic();
+
+        lo.a();
+
+    }
 }
