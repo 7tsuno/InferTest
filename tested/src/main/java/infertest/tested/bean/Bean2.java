6a84c0ffacd735b4437d51d13790c4b156c25a9e
nakayama <a@a>
2017-06-12 11:43:18 +0900

    aa

--- tested/src/main/java/infertest/tested/bean/Bean2.java
+++ tested/src/main/java/infertest/tested/bean/Bean2.java
@@ -1,5 +1,8 @@
 package infertest.tested.bean;
 
+import lombok.Data;
+
+@Data
 public class Bean2 {
 
     private String code = null;
@@ -8,27 +11,4 @@ public class Bean2 {
 
     private String code3;
 
-    public String getCode2() {
-        return code2;
-    }
-
-    public void setCode2(String code2) {
-        this.code2 = code2;
-    }
-
-    public String getCode3() {
-        return code3;
-    }
-
-    public void setCode3(String code3) {
-        this.code3 = code3;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
 }
